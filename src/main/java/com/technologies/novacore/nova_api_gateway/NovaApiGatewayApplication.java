b69d405826465c67866cfa5c509075547f921449
Pedro Manuel Diaz Santa Maria <pdiazsan@emeal.nttdata.com>
2025-08-14 16:39:32 -0500

    Initial commit

---
+++ src/main/java/com/technologies/novacore/nova_api_gateway/NovaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.technologies.novacore.nova_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technologies/novacore/nova_api_gateway/NovaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technologies.novacore.nova_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
